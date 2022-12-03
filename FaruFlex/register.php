b9d6979f7029962dcb3195950881dc7787f4848e
Pascal488 <maxpascal03@gmail.com>
2022-12-03 22:39:25 +0300

    :Add a php webapp clone of netflix init

---
+++ FaruFlex/register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="column">
+            <h1>Register here</h1>
+            <form action="" method="post">
+                <p>
+                <label for="firstname">FirstName</label> <br>
+                <input type="text">
+                </p>
+                <p>
+                <label for="firstname">FirstName</label> <br>
+                <input type="text">
+                </p> <p>
+                <label for="firstname">FirstName</label> <br>
+                <input type="text">
+                </p> <p>
+                <label for="firstname">FirstName</label> <br>
+                <input type="text">
+                </p> 
+                <button>
+                    Register
+                </button>
+            
+                
+                
+
+            </form>
+        </div>
+    </div>
+</body>
+</html>
